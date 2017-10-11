ba09444dedfa1575923bf0b7946b3097fea7187d
Tylor Steinberger <tylor@tryfynd.com>
2017-10-11 19:17:08 -0400

    fix: ensure declarations are not generated

--- src/cli/browser/defaultConfig.ts
+++ src/cli/browser/defaultConfig.ts
@@ -8,7 +8,7 @@ const cwd = process.cwd()
 const HtmlOptions = {
   filename: `index.html`,
   inject: `body`,
-  template: path.join(__dirname, '../../../templates', 'index.ejs'),
+  template: path.join(__dirname, '../../../templates', 'index.ejs')
 }
 
 const HtmlWebpackPlugin = require('html-webpack-plugin')
@@ -18,29 +18,38 @@ export const defaultConfig = {
   entry: {} as Record<string, string>,
   plugins: [
     new HtmlWebpackPlugin(HtmlOptions),
-    new ForkTsCheckerWebpackPlugin({ checkSyntacticErrors: true, tsconfig: CONFIG_PATH }),
+    new ForkTsCheckerWebpackPlugin({ checkSyntacticErrors: true, tsconfig: CONFIG_PATH })
   ],
   module: {
     rules: [
       {
         test: /\.ts?$/,
-        use: ['ts-loader'],
-      },
-    ],
+        use: [
+          {
+            loader: 'ts-loader',
+            options: {
+              compilerOptions: {
+                declaration: false
+              }
+            }
+          }
+        ]
+      }
+    ]
   },
   resolve: {
     mainFields: ['module', 'jsnext:main', 'browser', 'main'],
     extensions: ['.ts', '.tsx', '.js', '.jsx', '.json'],
-    alias: {} as Record<string, string>,
+    alias: {} as Record<string, string>
   },
   output: {
     filename: '[name].js',
     libraryTarget: 'var',
     library: '[name]',
-    path: path.join(cwd, tempDir.name),
+    path: path.join(cwd, tempDir.name)
   },
   target: 'web',
   node: {
-    fs: 'empty',
-  },
+    fs: 'empty'
+  }
 }
